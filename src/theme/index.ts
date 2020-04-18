35cbc00546cfc3afb32daadc6c62f762b50f6256
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-18 21:14:47 +0200

    feat(themes): Mui themes added

---
+++ src/theme/index.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    typography: {
+        allVariants: {
+            color: '#FFFFFF',
+        },
+    },
+    palette: {
+        background: {
+            default: '#2E282A',
+        },
+    },
+});
