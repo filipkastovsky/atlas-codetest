0290aa2daed9b1d0ee9ea64b0c0a8a507140bde5
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-18 20:47:36 +0200

    feat: Add dev environment variable support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import env from 'dotenv';
+
+env.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
