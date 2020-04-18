1dafbe6a514dc946a56e6f23da0c3eea29ba4c5b
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-18 22:14:45 +0200

    feat(context): Provide AppContext to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { theme } from './theme';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 import { ApolloProvider } from 'react-apollo';
 import { graphqlClient as client } from './graphql/graphqlClient';
+import { AppContext } from './context/AppContext';
 
 export const App: React.FC = () => (
-    <ApolloProvider {...{ client }}>
-        <ThemeProvider {...{ theme }}>
-            <AppRouter />
-            <CssBaseline />
-        </ThemeProvider>
-    </ApolloProvider>
+    <AppContext.Provider value={{}}>
+        <ApolloProvider {...{ client }}>
+            <ThemeProvider {...{ theme }}>
+                <AppRouter />
+                <CssBaseline />
+            </ThemeProvider>
+        </ApolloProvider>
+    </AppContext.Provider>
 );
