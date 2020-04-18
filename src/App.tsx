fafb4959989637bf8550d113f272e2b9cd73076d
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-18 21:23:04 +0200

    feat(gql): Provide gql to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { AppRouter } from './components/routers/AppRouter';
 import { theme } from './theme';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { ApolloProvider } from 'react-apollo';
+import { graphqlClient as client } from './graphql/graphqlClient';
 
 export const App: React.FC = () => (
-    <ThemeProvider {...{ theme }}>
-        <AppRouter />
-        <CssBaseline />
-    </ThemeProvider>
+    <ApolloProvider {...{ client }}>
+        <ThemeProvider {...{ theme }}>
+            <AppRouter />
+            <CssBaseline />
+        </ThemeProvider>
+    </ApolloProvider>
 );
