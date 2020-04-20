043b818033a84862ed03350a2cbdcc733b9edbef
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-20 12:32:46 +0200

    feat(ui): DashboardContainer component

---
+++ src/components/pages/Dashboard/_components/DashboardContainer.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Paper } from '@material-ui/core';
+
+export const DashboardContainer = styled(Paper)`
+    display: flex;
+    width: 80%;
+    height: 80%;
+`;
