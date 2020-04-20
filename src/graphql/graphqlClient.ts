128506de39262b0fffd79c9e7ee00b1928c02b6f
filipkastovsky <devfilipkastovsky@gmail.com>
2020-04-20 09:55:35 +0200

    chore: Explicit graphQlClient url

--- src/graphql/graphqlClient.ts
+++ src/graphql/graphqlClient.ts
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import { ErrorResponse } from 'apollo-link-error';
 
 export const graphqlClient = new ApolloClient({
-    uri: process.env.API_URL,
+    uri: 'https://react-test.atlasconsulting.cz',
     onError(error: ErrorResponse) {
         console.error('Unhandled GQL error', error);
     },
